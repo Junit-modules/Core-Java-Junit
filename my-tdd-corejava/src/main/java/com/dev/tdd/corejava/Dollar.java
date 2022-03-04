f2c149ad29cb1f739177ec48e4e6b99fbb2cf6b1
GANESAPRABU NAVAMANIRAJAN <ganesaprabunavamanirajan@GANESAPRABUs-MacBook-Air.local>
2022-03-04 07:58:46 +0530

    Added a parent class Money and extends this class from Dollar & Franc

--- my-tdd-corejava/src/main/java/com/dev/tdd/corejava/Dollar.java
+++ my-tdd-corejava/src/main/java/com/dev/tdd/corejava/Dollar.java
@@ -2,8 +2,7 @@ package com.dev.tdd.corejava;
 
 import java.util.Objects;
 
-public class Dollar {
-    private int amount;
+public class Dollar extends Money{
 
     public Dollar(int amount) {
         this.amount = amount;
@@ -11,18 +10,5 @@ public class Dollar {
 
     public Dollar times(int times) {
         return new Dollar(this.amount * times);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Dollar dollar = (Dollar) o;
-        return amount == dollar.amount;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(amount);
     }
 }
